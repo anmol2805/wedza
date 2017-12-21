013cc229628dc41d7952469df58ebfb5c7f4f7ca
anmol2805 <saxenaanmol28@gmail.com>
2017-12-21 10:50:03 +0530

    Introduce yourself started

---
+++ app/src/main/java/com/anmol/wedza/IntroduceYourselfActivity.java
@@ -0,0 +1,13 @@
+package com.anmol.wedza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroduceYourselfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduce_yourself);
+    }
+}
