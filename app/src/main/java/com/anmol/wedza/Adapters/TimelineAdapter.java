961e51f991b047b54f8ddfbe10c5556896e0ddd6
anmol2805 <saxenaanmol28@gmail.com>
2017-12-26 20:28:47 +0530

    timeline almost done

--- app/src/main/java/com/anmol/wedza/Adapters/TimelineAdapter.java
+++ app/src/main/java/com/anmol/wedza/Adapters/TimelineAdapter.java
@@ -2,6 +2,7 @@ package com.anmol.wedza.Adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,8 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 import com.anmol.wedza.Model.Timeline;
+import com.anmol.wedza.R;
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -47,7 +52,16 @@ public class TimelineAdapter extends ArrayAdapter<Timeline> {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             //LayoutInflater inflater = context.getLayoutInflater();
             View v = inflater.inflate(resource,null);
-
+            ImageView mediaimg = (ImageView)v.findViewById(R.id.mediaphoto);
+            VideoView mediavid = (VideoView)v.findViewById(R.id.mediavideo);
+            if(timelines.get(position).getMediatype().contains("image")){
+                mediaimg.setVisibility(View.VISIBLE);
+                Glide.with(getContext()).load(timelines.get(position).getMedialink()).into(mediaimg);
+            }
+            else if(timelines.get(position).getMediatype().contains("video")){
+                mediavid.setVisibility(View.VISIBLE);
+                mediavid.setVideoURI(Uri.parse(timelines.get(position).getMedialink()));
+            }
 
             return v;
         }
