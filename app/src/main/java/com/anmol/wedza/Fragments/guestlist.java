8d4bf6afd855f595d3290a69ad5b0ccbf2645d2a
anmol2805 <saxenaanmol28@gmail.com>
2017-12-27 11:24:35 +0530

    guestlist started

--- app/src/main/java/com/anmol/wedza/Fragments/guestlist.java
+++ app/src/main/java/com/anmol/wedza/Fragments/guestlist.java
@@ -1,11 +1,51 @@
 package com.anmol.wedza.Fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.anmol.wedza.R;
+
+import java.util.List;
 
 /**
  * Created by anmol on 12/27/2017.
  */
 
 public class guestlist extends Fragment {
+    ListView glv;
+    Button everyone,tgr,tbr;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View vi = inflater.inflate(R.layout.guestlist,container,false);
+        glv = (ListView)vi.findViewById(R.id.guestlv);
+        everyone = (Button)vi.findViewById(R.id.everyone);
+        tgr = (Button)vi.findViewById(R.id.teamgr);
+        tbr = (Button)vi.findViewById(R.id.teambr);
+        everyone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+        tgr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        tbr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return vi;
+    }
 }
