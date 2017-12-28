4f8c3ac09d5395bfca80ce72801dabea9dbdad34
anmol2805 <saxenaanmol28@gmail.com>
2017-12-28 19:42:26 +0530

    Gallery adapter started

---
+++ app/src/main/java/com/anmol/wedza/Adapters/GalleryAdapter.java
@@ -0,0 +1,31 @@
+package com.anmol.wedza.Adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by anmol on 12/28/2017.
+ */
+
+public class GalleryAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
