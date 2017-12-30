71e17a408613599b49c7cccda32f21e1833abc11
anmol2805 <saxenaanmol28@gmail.com>
2017-12-30 08:38:24 +0530

    stories recycler started

--- app/src/main/java/com/anmol/wedza/Fragments/story.java
+++ app/src/main/java/com/anmol/wedza/Fragments/story.java
@@ -3,9 +3,14 @@ package com.anmol.wedza.Fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.anmol.wedza.R;
 
 /**
@@ -13,10 +18,19 @@ import com.anmol.wedza.R;
  */
 
 public class story extends Fragment {
+    Button liked,wishes;
+    RecyclerView listimg;
+    TextView storycontent;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View vi = inflater.inflate(R.layout.story,container,false);
+        liked = (Button)vi.findViewById(R.id.likedit);
+        wishes = (Button)vi.findViewById(R.id.wishes);
+        storycontent = (TextView)vi.findViewById(R.id.storycontent);
+        listimg = (RecyclerView)vi.findViewById(R.id.listimg);
+        listimg.setHasFixedSize(true);
+        listimg.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false));
         return vi;
     }
 }
---
+++ app/src/main/java/com/anmol/wedza/Interfaces/ItemClickListener.java
@@ -0,0 +1,9 @@
+package com.anmol.wedza.Interfaces;
+
+/**
+ * Created by anmol on 9/3/2017.
+ */
+
+public interface ItemClickListener {
+    void onItemClick(int pos);
+}
