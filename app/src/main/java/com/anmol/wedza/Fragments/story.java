a606bca3cc41c1ad82f7cc8048a1961d6d484727
anmol2805 <saxenaanmol28@gmail.com>
2017-12-30 08:15:21 +0530

    stories layout done

--- app/src/main/java/com/anmol/wedza/Fragments/story.java
+++ app/src/main/java/com/anmol/wedza/Fragments/story.java
@@ -1,10 +1,22 @@
 package com.anmol.wedza.Fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.anmol.wedza.R;
 
 /**
  * Created by anmol on 12/29/2017.
  */
 
 public class story extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View vi = inflater.inflate(R.layout.story,container,false);
+        return vi;
+    }
 }
