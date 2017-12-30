c4033c963231fea6333561c89dec5d3724755bb3
anmol2805 <saxenaanmol28@gmail.com>
2017-12-30 09:54:56 +0530

    adapter and layout almost set

--- app/src/main/java/com/anmol/wedza/Adapters/StoryimageAdapter.java
+++ app/src/main/java/com/anmol/wedza/Adapters/StoryimageAdapter.java
@@ -1,16 +1,21 @@
 package com.anmol.wedza.Adapters;
 
 import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.anmol.wedza.Interfaces.ItemClickListener;
 import com.anmol.wedza.Model.Storyimage;
 import com.anmol.wedza.R;
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -36,8 +41,25 @@ public class StoryimageAdapter extends RecyclerView.Adapter<StoryimageAdapter.My
     }
 
     @Override
-    public void onBindViewHolder(StoryimageAdapter.MyViewHolder holder, int position) {
-
+    public void onBindViewHolder(final StoryimageAdapter.MyViewHolder holder, int position) {
+        if(storyimages.get(position).getMediatype().contains("image")){
+            holder.mimg.setVisibility(View.VISIBLE);
+            Glide.with(c).load(storyimages.get(position).getMedialink()).into(holder.mimg);
+        }
+        else if(storyimages.get(position).getMediatype().contains("video")){
+            holder.mvid.setVisibility(View.VISIBLE);
+            MediaController mediaController = new MediaController(c);
+            mediaController.setAnchorView(holder.mvid);
+            holder.mvid.setMediaController(mediaController);
+            holder.mvid.setVideoURI(Uri.parse(storyimages.get(position).getMedialink()));
+            holder.mvid.requestFocus();
+            holder.mvid.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    holder.mvid.start();
+                }
+            });
+        }
     }
 
     @Override
@@ -45,12 +67,14 @@ public class StoryimageAdapter extends RecyclerView.Adapter<StoryimageAdapter.My
         return storyimages.size();
     }
     public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        ImageView img;
+        ImageView mimg;
+        VideoView mvid;
         private ItemClickListener mitemClickListener;
         public MyViewHolder(View itemView,ItemClickListener itemClickListener) {
             super(itemView);
             mitemClickListener = itemClickListener;
-            //img = (ImageView)itemView.findViewById(R.id.imageview);
+            mimg = (ImageView)itemView.findViewById(R.id.mediaimg);
+            mvid = (VideoView)itemView.findViewById(R.id.mediavid);
             itemView.setOnClickListener(this);
         }
 
