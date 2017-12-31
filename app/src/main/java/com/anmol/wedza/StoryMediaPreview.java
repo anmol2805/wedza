0069d2d88095301d0e88aac56628464e9e6f3be3
anmol2805 <saxenaanmol28@gmail.com>
2017-12-31 08:31:01 +0530

    todays first commit

---
+++ app/src/main/java/com/anmol/wedza/StoryMediaPreview.java
@@ -0,0 +1,14 @@
+package com.anmol.wedza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StoryMediaPreview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mediashow);
+
+    }
+}
