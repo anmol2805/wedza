244d4d1652f827f797c3e267431d8663c1971c22
anmol2805 <saxenaanmol28@gmail.com>
2018-01-08 08:17:41 +0530

    event to be started

--- app/src/main/java/com/anmol/wedza/HomeActivity.java
+++ app/src/main/java/com/anmol/wedza/HomeActivity.java
@@ -134,16 +134,12 @@ public class HomeActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
