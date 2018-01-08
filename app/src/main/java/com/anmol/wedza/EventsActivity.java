d8c792f871a6805c3d52d5466bf9b35d23c1043d
anmol2805 <saxenaanmol28@gmail.com>
2018-01-08 19:32:17 +0530

    event almost started

--- app/src/main/java/com/anmol/wedza/EventsActivity.java
+++ app/src/main/java/com/anmol/wedza/EventsActivity.java
@@ -3,34 +3,83 @@ package com.anmol.wedza;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.anmol.wedza.Adapters.EventsAdapter;
+import com.anmol.wedza.Model.Event;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class EventsActivity extends AppCompatActivity {
 
     FirebaseAuth auth = FirebaseAuth.getInstance();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    List<Event> events = new ArrayList<>();
+    EventsAdapter eventsAdapter;
+    ListView eventlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+        eventlist = (ListView)findViewById(R.id.eventlist);
         db.collection("users").document(auth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 String weddingid = task.getResult().getString("currentwedding");
-                db.collection("weddings").document(weddingid).collection("events").orderBy("time").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                getteam(weddingid);
+            }
+        });
+    }
 
-                    }
-                });
+    private void getteam(final String weddingid) {
+        db.collection("weddings").document(weddingid).collection("users").document(auth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                String team = task.getResult().getString("team");
+                getevent(weddingid,team);
+            }
+        });
+    }
+
+    private void getevent(String weddingid, String team) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date = sdf.format(new Date());
+        java.sql.Timestamp timestamp = java.sql.Timestamp.valueOf(date);
+        db.collection("weddings").document(weddingid)
+                .collection("events")
+                .whereEqualTo("team",team)
+                .orderBy("time")
+                .whereGreaterThanOrEqualTo("time",timestamp).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                events.clear();
+                for(DocumentSnapshot doc:documentSnapshots.getDocuments()){
+                    String eventname = doc.getString("eventname");
+                    String eventdes = doc.getString("eventdes");
+                    String eventimg = doc.getString("eventimg");
+                    String eventlocation = doc.getString("eventlocation");
+                    String team = doc.getString("team");
+                    Timestamp time = (Timestamp) doc.getDate("time");
+                    Event event = new Event(eventname,eventdes,eventimg,eventlocation,team,time);
+                    events.add(event);
+                }
+                eventsAdapter = new EventsAdapter(EventsActivity.this,R.layout.eventlayout,events);
+                eventlist.setAdapter(eventsAdapter);
+
             }
         });
     }
