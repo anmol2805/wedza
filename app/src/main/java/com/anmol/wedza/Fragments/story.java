6a8b2b8993039668b5824caccd6665d4ba2161ce
anmol2805 <saxenaanmol28@gmail.com>
2018-01-12 08:44:38 +0530

    story like done

--- app/src/main/java/com/anmol/wedza/Fragments/story.java
+++ app/src/main/java/com/anmol/wedza/Fragments/story.java
@@ -28,13 +28,18 @@ import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by anmol on 12/29/2017.
@@ -48,6 +53,7 @@ public class story extends Fragment {
     StoryimageAdapter storyimageAdapter;
     List<Storyimage> storyimages;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth auth = FirebaseAuth.getInstance();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -70,6 +76,50 @@ public class story extends Fragment {
         };
         loadmedia();
         loadcontent();
+        db.collection("users").document(auth.getCurrentUser().getUid()).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+                if(documentSnapshot!=null && documentSnapshot.exists()){
+                    String weddingid = documentSnapshot.getString("currentwedding");
+                    db.collection("weddings")
+                            .document(weddingid)
+                            .collection("storylikes")
+                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                        @Override
+                        public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                            for(DocumentSnapshot doc:documentSnapshots.getDocuments()){
+                                if(doc.getId().contains(auth.getCurrentUser().getUid())){
+                                    liked.setVisibility(View.INVISIBLE);
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        liked.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.collection("users").document(auth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        Map<String,Object> map = new HashMap<>();
+                        map.put("like",true);
+                        String weddingid = task.getResult().getString("currentwedding");
+                        db.collection("weddings")
+                                .document(weddingid)
+                                .collection("storylikes")
+                                .document(auth.getCurrentUser().getUid()).set(map);
+                    }
+                });
+            }
+        });
+        wishes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         return vi;
     }
 
